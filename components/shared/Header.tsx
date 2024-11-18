9fd342333518ba626bc17e9ad040faa4d90d6ba9
Srinivasa <srinivasaperisetla@gmail.com>
2024-11-18 08:45:02 -0800

    Added Header Footer and HomePage files

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,11 @@
-import { SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
+
 
 const Header = () => {
   return (
@@ -15,7 +18,18 @@ const Header = () => {
             />
           </Link>
 
+          <SignedIn>
+            <nav className='md:flex-between hidden w-full max-w-xs'>
+              <NavItems />
+            </nav>
+            
+          </SignedIn>
+
           <div className='flex w-32 justify-end gap-3'>
+            <SignedIn>
+              <UserButton />
+              <MobileNav />
+            </SignedIn>
             <SignedOut>
               <Button asChild className='rounded-full' size = "lg">
                 <Link href="/sign-in">
